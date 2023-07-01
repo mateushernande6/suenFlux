b0cc0abe4016a5bd79e11dd2e4f89cf88d53570f
Mateus Hernandes <mateushernandes939@gmail.com>
2023-07-01 13:30:46 -0300

    fix: rename app

--- ios/suenFluxTests/suenFluxTests.m
+++ ios/suenFluxTests/suenFluxTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface smartMoneyTests : XCTestCase
+@interface suenFluxTests : XCTestCase
 
 @end
 
-@implementation smartMoneyTests
+@implementation suenFluxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
