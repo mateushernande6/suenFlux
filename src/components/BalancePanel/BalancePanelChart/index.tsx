a1b349a2b18f2dfc0d8c5bb5dd67b902c0b8c62b
Mateus Hernandes <mateushernandes939@gmail.com>
2023-07-02 16:00:21 -0300

    feat: created initial graphic and service to get datas to graphic

--- src/components/BalancePanel/BalancePanelChart/index.tsx
+++ src/components/BalancePanel/BalancePanelChart/index.tsx
@@ -1,13 +1,34 @@
 import {View, StyleSheet} from 'react-native';
 import React from 'react';
 
+import {BarChart} from 'react-native-svg-charts';
+
 const BalancePanelChart = () => {
-  return <View style={styles.container}></View>;
+  const data = [150, -80, 400, 85, 150, 100, 350];
+  return (
+    <View style={styles.container}>
+      <BarChart
+        style={styles.chart}
+        data={data}
+        svg={{
+          fill: 'rgba(0,0,0,0.1)',
+          stroke: 'rgba(0,0,0,0.1)',
+          strokeWidth: 1,
+        }}
+        contentInset={{top: 0, bottom: 0}}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingHorizontal: 20,
+    marginTop: -20,
+    marginBottom: 3,
+  },
+  chart: {
+    height: 60,
   },
 });
 
--- src/services/Balance.ts
+++ src/services/Balance.ts
@@ -1,5 +1,36 @@
-export const getBalance = async realm => {
+import dayjs from '../vendors/dayjs';
+import _ from 'lodash';
+
+export const getBalance = async (realm, untilDays = 0) => {
   let entries = await realm.objects('Entry');
 
+  if (untilDays > 0) {
+    const date = dayjs().subtract(untilDays, 'days').toDate();
+
+    entries = entries.filtered('entryAt < $0', date);
+  }
+
   return entries.sum('amount');
+};
+
+export const getBalanceSumByDdate = async (realm, days) => {
+  // const startBalance = await getBalance(realm, days);
+
+  let entries = await realm.objects('Entry');
+
+  if (days > 0) {
+    const date = dayjs().subtract(days, 'days').toDate();
+
+    entries = entries.filtered('entryAt >= $0', date);
+  }
+
+  entries = entries.sorted('entryAt');
+
+  entries = _(entries).groupBy(({entryAt}) =>
+    dayjs(entryAt).format('YYYYMMDD'),
+  );
+
+  console.log(JSON.stringify('teste -> ', entries));
+
+  return entries;
 };
